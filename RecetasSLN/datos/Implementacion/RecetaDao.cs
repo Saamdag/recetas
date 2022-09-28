aac606045785cef2d2cb8bfbe569733cda22218a
Saamdag <samirbrusasca@outlook.com>
2022-09-28 20:17:58 -0300

    clase

---
+++ RecetasSLN/datos/Implementacion/RecetaDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RecetasSLN.datos;
+
+namespace RecetasSLN.datos.Implementacion
+{
+    internal class RecetaDao : IReceta
+    {
+        public int obtenerProxNum()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RecetasSLN/datos/helperDb.cs
@@ -0,0 +1,52 @@
+﻿using RecetasSLN.dominio;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace RecetasSLN.datos
+{
+    internal class helperDb
+    {
+        private static helperDb instancia;
+        private DAO DAO = new DAO();
+
+        private helperDb()
+        {
+            
+        }
+
+        public static helperDb obtenerInstancia()
+        {
+            if(instancia== null)
+                instancia = new helperDb();
+            return instancia;
+        }
+
+        public DataTable listarProductos()
+        {
+            return DAO.listarIngredientes();
+
+        }
+
+        public bool confirmarReceta(Receta oReceta)
+        {
+            return DAO.ejecutarInsert(oReceta);
+
+        }
+
+        public void consultarProximo(string nombreSp,string nombreParamOut)
+        {
+
+        }
+
+
+
+
+    }
+}
